78557fdf02078b68f309e316969003c1ba6d6f85
Komolafe Temiloluwa Ayomide <103121606+T3mmyKay@users.noreply.github.com>
2026-09-04 01:22:38 +0100

    chore(ef): update model snapshot for active state

--- T3mmyvsa.Web/Data/Migrations/AppDbContextModelSnapshot.cs
+++ T3mmyvsa.Web/Data/Migrations/AppDbContextModelSnapshot.cs
@@ -134,6 +134,7 @@ namespace T3mmyvsa.Data.Migrations
                     b.Property<string>("Email").HasMaxLength(256).HasColumnType("nvarchar(256)");
                     b.Property<bool>("EmailConfirmed").HasColumnType("bit");
                     b.Property<string>("FirstName").HasColumnType("nvarchar(max)");
+                    b.Property<bool>("IsActive").HasColumnType("bit");
                     b.Property<string>("LastName").HasColumnType("nvarchar(max)");
                     b.Property<bool>("LockoutEnabled").HasColumnType("bit");
                     b.Property<DateTimeOffset?>("LockoutEnd").HasColumnType("datetimeoffset");
