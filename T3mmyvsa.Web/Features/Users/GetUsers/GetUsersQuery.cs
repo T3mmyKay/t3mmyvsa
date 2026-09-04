2da8c1b8bfceff5d47a9f8d8d06e2d399e83f18e
Komolafe Temiloluwa Ayomide <103121606+T3mmyKay@users.noreply.github.com>
2026-09-04 01:20:51 +0100

    fix(api): standardize user list query parameters

--- T3mmyvsa.Web/Features/Users/GetUsers/GetUsersQuery.cs
+++ T3mmyvsa.Web/Features/Users/GetUsers/GetUsersQuery.cs
@@ -1,13 +1,13 @@
-using T3mmyvsa.Models.Shared;
-using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
+using T3mmyvsa.Models.Shared;
 
 namespace T3mmyvsa.Features.Users.GetUsers;
 
 public record GetUsersQuery(
-    string? Search,
-    UserSortColumn? SortColumn,
-    SortOrder? SortOrder,
-    int? Page,
-    int? PageSize
+    [FromQuery(Name = "search")] string? Search,
+    [FromQuery(Name = "sort_column")] UserSortColumn? SortColumn,
+    [FromQuery(Name = "sort_order")] SortOrder? SortOrder,
+    [FromQuery(Name = "page"), Range(1, int.MaxValue)] int? Page,
+    [FromQuery(Name = "per_page"), Range(1, 100)] int? PageSize
 ) : IQuery<PaginatedResponse<UserResponse>>;
