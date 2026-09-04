4d929d03c6295d66715d31cdcf08a1678257a845
Komolafe Temiloluwa Ayomide <103121606+T3mmyKay@users.noreply.github.com>
2026-09-04 01:19:53 +0100

    fix(validation): align registration with identity password policy

--- T3mmyvsa.Web/Features/Auth/Register/RegisterCommand.cs
+++ T3mmyvsa.Web/Features/Auth/Register/RegisterCommand.cs
@@ -2,7 +2,7 @@ namespace T3mmyvsa.Features.Auth.Register;
 
 public record RegisterCommand(
     [Required, EmailAddress] string Email,
-    [Required] string Password,
-    [Required] string FirstName,
-    [Required] string LastName
+    [Required, StringLength(100, MinimumLength = 10)] string Password,
+    [Required, StringLength(100, MinimumLength = 2)] string FirstName,
+    [Required, StringLength(100, MinimumLength = 2)] string LastName
 ) : ICommand;
