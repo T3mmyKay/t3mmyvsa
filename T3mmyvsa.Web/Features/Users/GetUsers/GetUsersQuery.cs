955226bf0e7142b27145585496d44ade5899e735
Komolafe Temiloluwa Ayomide <103121606+T3mmyKay@users.noreply.github.com>
2026-09-04 01:26:53 +0100

    fix(openapi): bind user query metadata on properties

--- T3mmyvsa.Web/Features/Users/GetUsers/GetUsersQuery.cs
+++ T3mmyvsa.Web/Features/Users/GetUsers/GetUsersQuery.cs
@@ -1,13 +1,22 @@
-using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
 using T3mmyvsa.Models.Shared;
 
 namespace T3mmyvsa.Features.Users.GetUsers;
 
-public record GetUsersQuery(
-    [FromQuery(Name = "search")] string? Search,
-    [FromQuery(Name = "sort_column")] UserSortColumn? SortColumn,
-    [FromQuery(Name = "sort_order")] SortOrder? SortOrder,
-    [FromQuery(Name = "page"), Range(1, int.MaxValue)] int? Page,
-    [FromQuery(Name = "per_page"), Range(1, 100)] int? PageSize
-) : IQuery<PaginatedResponse<UserResponse>>;
+public sealed record GetUsersQuery : IQuery<PaginatedResponse<UserResponse>>
+{
+    [FromQuery(Name = "search")]
+    public string? Search { get; init; }
+
+    [FromQuery(Name = "sort_column")]
+    public UserSortColumn? SortColumn { get; init; }
+
+    [FromQuery(Name = "sort_order")]
+    public SortOrder? SortOrder { get; init; }
+
+    [FromQuery(Name = "page")]
+    public int? Page { get; init; }
+
+    [FromQuery(Name = "per_page")]
+    public int? PageSize { get; init; }
+}
