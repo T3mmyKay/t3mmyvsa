ed24ac1cbd88586f93218ca797a5c87ac6816349
Komolafe Temiloluwa Ayomide <103121606+T3mmyKay@users.noreply.github.com>
2026-09-04 01:25:16 +0100

    fix(validation): tighten administrative user updates

--- T3mmyvsa.Web/Features/Users/UpdateUser/UpdateUserCommand.cs
+++ T3mmyvsa.Web/Features/Users/UpdateUser/UpdateUserCommand.cs
@@ -3,10 +3,13 @@ namespace T3mmyvsa.Features.Users.UpdateUser;
 public record UpdateUserCommand : ICommand
 {
     public required Guid UserId { get; init; }
-    [Required]
+
+    [Required, StringLength(100, MinimumLength = 2)]
     public required string FirstName { get; init; }
-    [Required]
+
+    [Required, StringLength(100, MinimumLength = 2)]
     public required string LastName { get; init; }
-    [Required]
+
+    [Required, Phone]
     public required string PhoneNumber { get; init; }
 }
